8cc1ce631d3fcd64be6c9085e084468f321a0494
王喆 <nasa127@gmail.com>
2016-11-05 23:51:37 +0800

    easings

---
+++ src/modules/anime/views/easings.jsx
@@ -0,0 +1,19 @@
+/**
+ * create by nasa.wang
+ */
+
+import React from 'react'
+
+const Easings = React.createClass({
+    render() {
+        return (
+            <div>
+                ANIME easings view
+            </div>
+
+        )
+    }
+
+})
+
+export default Easings
